ed158cc0dae5cfc3a371628d83195db03cf4c61c
ryanbollins <ryanc91003@gmail.com>
2024-11-23 12:47:07 -0500

    kjmkm
    knkk

--- project.js
+++ project.js
@@ -12,3 +12,19 @@ document.querySelectorAll(".publish").forEach(function(element) {
         sound.play();
     });
 });
+
+function validateForm() {
+    if (document.forms[0].bookName.value == "") {
+        alert("Title field cannot be empty.");
+        return false;
+    }
+    if (document.forms[0].bookAuthor.value == "") {
+        alert("author field cannot be empty.");
+        return false;
+    }
+    if (document.forms[0].review.value == "") {
+        alert("Review field cannot be empty.");
+        return false;
+    }
+}
+
